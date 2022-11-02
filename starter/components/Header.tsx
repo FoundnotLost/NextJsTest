7e0fbd35e005687882a98f074e16a3c52fc35cdc
Jason <jasonmadruga25@gmail.com>
2022-11-02 12:58:19 -0500

    added package react-social-icons

---
+++ starter/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons';
+
+type Props = {}
+
+function Header({}: Props) {
+  return (
+    <header>
+        <div>
+        {/* Media Icons */}
+        </div>
+        </header>
+  )
+}
+
+export default Header
