44ea6a05c2fe5d4b83aa30da5b5db0f238582152
Jason <jasonmadruga25@gmail.com>
2022-11-02 12:50:30 -0500

    Start of my build working with Next.js TS Tailwind

---
+++ starter/pages/index.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Jason's Portfolio</title>
+      </Head>
+
+{/* Header */}
+<Header /> 
+{/* Hero */} 
+
+{/* About */}
+
+{/* Experience */}
+
+{/* Projects */}
+
+{/* Contact Me */}
+
+  
+  
+  
+   </div>
+  )
+}
