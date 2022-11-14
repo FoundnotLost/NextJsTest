272d50549b6ed6592c0e277c0712328872742f1b
Jason <jasonmadruga25@gmail.com>
2022-11-14 11:26:09 -0600

    removed unneeded files

---
+++ starter/sanity/schemas/projects.js
@@ -0,0 +1,54 @@
+export default {
+    name: 'Experience',
+    title: 'experience',
+    type: 'document',
+    fields: [
+      {
+        name: 'jobTitle',
+        title: 'JobTitle',
+        type: 'string',
+      },
+      {
+        name: 'role',
+        title: 'Role',
+        type: 'string',
+      },
+      {
+        name: 'companyImage',
+        title: 'CompanyImage',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+      {
+        name: 'dateStarted',
+        title: 'DateStarted',
+        type: 'string',
+      },
+      {
+        name: 'dateEnded',
+        title: 'DateEnded',
+        type: 'string',
+      },
+      {
+        name: 'isCurrentlyWorkingHere',
+        title: 'IsCurrentlyWorkingHere',
+        type: 'Boolean',
+      },
+      {
+        name: 'technologies',
+        title: 'Techologies',
+        type: 'array',
+        of: [{ type: 'reference', to: { type: 'skill'} }],
+      },
+      {
+        name: 'points',
+        title: 'Points',
+        type: 'array',
+        of: [{ type: 'string'}],
+      },
+     
+    ],
+  }
+  
